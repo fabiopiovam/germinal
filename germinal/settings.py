b50f47e4d60d4f76f61008de12b19695c07f579d
laborautonomo <fabio@laborautonomo.org>
2014-09-27 23:17:39 -0300

    [chg] reorganized static files

--- germinal/settings.py
+++ germinal/settings.py
@@ -121,7 +121,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'assets'),
+    os.path.join(BASE_DIR, 'products/static'),
     os.path.join(BASE_DIR, 'newsletter/static'),
 )
 
